0eb36f2e13e0fbb91c4df916e1655724c8f0b916
KafiRijal <kafirs75@gmail.com>
2024-04-28 15:14:48 +0700

    project1

---
+++ db_puskesmas.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2024 at 10:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_puskesmas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelurahan`
+--
+
+CREATE TABLE `kelurahan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `kec_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelurahan`
+--
+
+INSERT INTO `kelurahan` (`id`, `nama`, `kec_id`) VALUES
+(1, 'Sukmajaya', 1),
+(2, 'Mekar Jaya', 1),
+(3, 'Bakti Jaya', 2),
+(4, 'Abadi Jaya', 2),
+(5, 'Tirta Jaya', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paramedik`
+--
+
+CREATE TABLE `paramedik` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `tmp_lahir` varchar(30) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `kategori` enum('Dokter','Perawat','Administrasi') NOT NULL,
+  `telpon` varchar(20) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `unit_kerja_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `paramedik`
+--
+
+INSERT INTO `paramedik` (`id`, `nama`, `gender`, `tmp_lahir`, `tgl_lahir`, `kategori`, `telpon`, `alamat`, `unit_kerja_id`) VALUES
+(4, 'kafi rijal', 'L', 'Bandung', '2024-04-28', 'Dokter', '085156629197', 'Disana', 6),
+(5, 'Takbir', 'L', 'Indramayu', '2024-04-28', 'Perawat', '085156629197', 'Disini', 7),
+(6, 'Daffa', 'L', 'Depok', '2024-04-28', 'Administrasi', '085156629197', 'Disitu', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(10) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `tmp_lahir` varchar(30) DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `gender` enum('L','P') DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `kelurahan_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `kode`, `nama`, `tmp_lahir`, `tgl_lahir`, `gender`, `email`, `alamat`, `kelurahan_id`) VALUES
+(1, 'P001', 'Kafi Rijal', '', '0000-00-00', 'L', 'kafi@gmail.com', 'Jalan Lapangan Member', 1),
+(2, 'P002', 'Kapoy', NULL, NULL, 'L', 'kapoy@gmail.com', 'Jalan Kampung Sidamukti', 5),
+(3, 'P003', 'Nayanika', NULL, NULL, 'P', 'nayanika@gmail.com', 'Jalan Bogol', 5),
+(4, 'P004', 'Zoya', NULL, NULL, 'P', 'zoya@gmail.com', 'Jalan Tanjung Pinang', 3),
+(5, 'P005', 'Daffa', NULL, NULL, 'L', 'daffa@gmail.com', 'Jalan Pondok', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksa`
+--
+
+CREATE TABLE `periksa` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `berat` double NOT NULL,
+  `tinggi` double NOT NULL,
+  `tensi` varchar(20) NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `pasien_id` int(11) NOT NULL,
+  `dokter_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `tanggal`, `berat`, `tinggi`, `tensi`, `keterangan`, `pasien_id`, `dokter_id`) VALUES
+(2, '2024-04-28', 60, 180, 'normal', 'Bagus', 2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_kerja`
+--
+
+CREATE TABLE `unit_kerja` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `unit_kerja`
+--
+
+INSERT INTO `unit_kerja` (`id`, `nama`) VALUES
+(6, 'Medis'),
+(7, 'Keperawatan'),
+(8, 'Administrasi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_unit_kerja` (`unit_kerja_id`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kode` (`kode`),
+  ADD KEY `fk_pasien_kelurahan` (`kelurahan_id`);
+
+--
+-- Indexes for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_periksa_pasien` (`pasien_id`),
+  ADD KEY `fk_periksa_dokter` (`dokter_id`);
+
+--
+-- Indexes for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `periksa`
+--
+ALTER TABLE `periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  ADD CONSTRAINT `fk_unit_kerja` FOREIGN KEY (`unit_kerja_id`) REFERENCES `unit_kerja` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD CONSTRAINT `fk_pasien_kelurahan` FOREIGN KEY (`kelurahan_id`) REFERENCES `kelurahan` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD CONSTRAINT `fk_periksa_dokter` FOREIGN KEY (`dokter_id`) REFERENCES `paramedik` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_periksa_pasien` FOREIGN KEY (`pasien_id`) REFERENCES `pasien` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
